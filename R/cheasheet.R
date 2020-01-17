3ce8adc9eb39084aa34d757030e3da73d87b67ab
danielkupka <d.kupka@hotmail.com>
2020-01-17 21:41:42 +0100

    files

---
+++ R/cheasheet.R
@@ -0,0 +1,26 @@
+
+
+#solution
+cs_strings <- function(variables) {
+  BROWSE("https://docs.google.com/viewer?url=https://github.com/rstudio/cheatsheets/raw/master/strings.pdf")
+}
+
+
+cs_reticulate <- function(variables) {
+  BROWSE("https://docs.google.com/viewer?url=https://github.com/rstudio/cheatsheets/raw/master/xplain.pdf&embedded=TRUE")
+}
+
+# &embedded=TRUE
+
+cs_reticulate()
+
+https://docs.google.com/viewer?url=http://geek-easy.com/wp-content/uploads/2012/01/TOP-15-ESSENTIAL-FREE-SOFTWARE.pdf%20&embedded=true
+
+
+https://thecoatlessprofessor.com/programming/r/creating-an-r-data-package/ 
+
+ch_reticulate("https://docs.google.com/viewer?url=https://github.com/rstudio/cheatsheets/raw/master/xplain.pdf&embedded=TRUE")
+
+test <- browseURL("https://docs.google.com/viewer?url=https://github.com/rstudio/cheatsheets/raw/master/xplain.pdf&embedded=TRUE")
+
+cs_
